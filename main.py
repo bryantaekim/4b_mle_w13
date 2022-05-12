056e38879ae7b4e36e52ba140bc2a63d5ff59e9f
Bryan Kim <bryantaekim@gmail.com>
2022-05-12 19:42:17 -0400

    adding a new main.py

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from transformers import pipeline
-sentiment_model = pipeline("Sentiment-Analysis")
-
-sentiment_query_sentence = get_random_comment(top_comments)
-sentiment = sentiment_model(sentiment_query_sentence)
-print(f"Sentiment test:" {sentiment_query_sentence} == {sentiment})
-
 from pydantic import BaseModel
-
 calss PredictionRequest(BaseModel):
 	query_string: str
 
 @app.post("my_endpoint"):
 def my_endpooint(request: PredictionRequest):
-	return "HuggingFace"
+	sentiment_model = pipeline("Sentiment-Analysis")
+
+	request = PredictionRequest(sentiment_query_sentence = "I love you")
+	sentiment = sentiment_model(sentiment_query_sentence)
+	print(f"Sentiment test:" {sentiment_query_sentence} == {sentiment})
+	
