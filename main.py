89828cd61b59d8c0a1224c0b393cebf9a43b6e9f
Bryan Kim <34020408+bryantaekim@users.noreply.github.com>
2022-05-12 19:51:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from transformers import pipeline
 from pydantic import BaseModel
-calss predictionRequest(BaseModel):
+class PredictionRequest(BaseModel):
 	query_string: str
 
 @app.post("my_endpoint"):
-def my_endpooint(request: predictionRequest):
+def my_endpooint(request: PredictionRequest):
 	sentiment_model = pipeline("Sentiment-Analysis")
 
-	request = predictionRequest(sentiment_query_sentence = "I love you")
+	request = PredictionRequest(sentiment_query_sentence = "I love you")
 	sentiment = sentiment_model(sentiment_query_sentence)
 	print(f"Sentiment test:" {sentiment_query_sentence} == {sentiment})
 	
